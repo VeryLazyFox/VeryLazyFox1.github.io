cb1d9fe792545f1c1dc0b8063d0948f6c35a57e4
Marina <1maryfox1@gmail.com>
2018-12-20 19:01:13 +0200

    first commit

---
+++ precache-manifest.17f78644bea0a700a78fe0b66f19dbd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f4f8669ed9c2c112870e",
+    "url": "/js/chunk-vendors.fd1f063d.js"
+  },
+  {
+    "revision": "5c27dfdacd7222315a1e",
+    "url": "/js/app.e62a2695.js"
+  },
+  {
+    "revision": "8871b0dd4d9fc9649819",
+    "url": "/js/about.a956700f.js"
+  },
+  {
+    "revision": "dcdc01edf560698fcbe3489b98d9113b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "694b4dde619d5157f0e7eed091775da6",
+    "url": "/fonts/fontello.694b4dde.woff"
+  },
+  {
+    "revision": "501ce09c42716a2f6e1503a25eb174c9",
+    "url": "/fonts/Montserrat-Regular2.501ce09c.woff2"
+  },
+  {
+    "revision": "5c27dfdacd7222315a1e",
+    "url": "/css/app.07496a4c.css"
+  }
+];
